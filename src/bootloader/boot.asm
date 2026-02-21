02ec4c2aa8bcaf5adbf3b3acc6b96c9e4a34cb10
upanshu-chilveri <chilveri.upanshu@.com>
2026-02-22 01:36:00 +0530

    reading messages from floppy, hello world!

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -18,7 +18,7 @@ bdb_dir_entries_count:      dw 0E0h
 bdb_total_sectorsL          dw 2880
 bdb_media_descriptor_type:  db 0F0h
 bdb_sectors_per_fat:        dw 9
-bdb_sectos_per_track:       dw 18
+bdb_sectors_per_track:      dw 18
 bdb_heads:                  dw 2
 bdb_hidden_sectors:         dd 0
 bdb_large_sector_count:     dd 0
@@ -48,6 +48,8 @@ start:
 puts:
     push si
     push ax
+    push bx
+
 
 .loop:
     lodsb           ;loads next character in al
@@ -61,6 +63,7 @@ puts:
     jmp .loop
 
 .done:
+    pop bx
     pop ax
     pop si
     ret
@@ -80,20 +83,155 @@ main:               ;main program entry point
     mov sp, 0X7C00          ;initalize to start of OS as stack moves downwards
 
 
+    ;adding something to read from floppy, fat
+    
+    ;need to initialize ebr_d_no to dl
+
+    mov [ebr_drive_number], dl          ;direct addressing of memory address
+
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+
+
+
+
     ;printing message now
 
     mov si, msg_hello
     call puts
 
-
+    cli
     hlt
 
+;
+;handling error handling
+;
+
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h                  ;interupt to wait for keypress
+    jmp 0FFFFh:0             ;boot to start of BIOS, to restart
+
+
+
+
+
+
+
 .halt:              ;used to halt if suppose code jumps after 1st halt
-    jmp .halt
+    cli
+    hlt
+
+
+;DISK ROUTINES - NO NEED TO UNDERSTAND SHIT
+
+
+
+;converts standard lba addressing to chs addressing, used by physical hard disk_reset
+
+lba_to_chs:
+    push ax
+    push dx
+
+
+    xor dx,dx
+    div word [bdb_sectors_per_track]
+
+
+    inc dx
+    mov cx, dx
+
+    xor dx, dx
+    div word [bdb_heads]
+
+    mov dh, dl
+    mov ch, al
+    shl ah, 6
+    or cl, ah
+
+    pop ax
+    mov dl, al
+    pop ax
+    ret
+
+
+;reading sectors from disk!!!!!!
+
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx
+    call lba_to_chs
+    pop ax
+
+    mov ah, 02h
+    mov di, 3           ;retry counter
+
+.retry:
+    pusha
+    stc
+    int 13h
+    jnc .done
+
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .done
+
+
+;if retry fails
+
+.fail:
+    jmp floppy_error
+
+
+.done:
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
+
+
+
+
+;resets the disk controller
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+
+
+
+
+
 
 
 
 msg_hello: db "Hello world!", ENDL, 0
+msg_read_failed: db 'Read from disk failed!', ENDL, 0
 
 
 
