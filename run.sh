b19a618512d44a38475df70ac372dcb76a364eb0
upanshu-chilveri <chilveri.upanshu@.com>
2026-02-23 01:38:28 +0530

    Improved ./run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@ make
 
 qemu-system-i386 -fda build/main_floppy.img
 
+echo
+echo "------------------------------------------------------------------------------------------------------------------"
+printf "\nPrinting from the C Program after quitting qemu\n\n"
+./build/tools/fat build/main_floppy.img "TEST    TXT"
